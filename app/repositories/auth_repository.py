5e76f9098db3c1ac4ae50b68dc7cf244217694d4
juantaborda27 <juantabordaacosta@gmail.com>
2026-08-21 07:19:45 -0500

    Organizacion Arquitectura

---
+++ app/repositories/auth_repository.py
@@ -0,0 +1,24 @@
+from app.core.supabase import supabase
+from app.schemas.auth import RegisterRequest, LoginRequest
+
+
+class AuthRepository:
+
+    def register_user(self, data: RegisterRequest):
+        return supabase.auth.sign_up({
+            "email": data.email,
+            "password": data.password,
+            "options": {
+                "data": {
+                    "nombre_completo": data.nombre_completo,
+                    "telefono": data.telefono,
+                    "ubicacion": data.ubicacion
+                }
+            }
+        })
+
+    def login_user(self, data: LoginRequest):
+        return supabase.auth.sign_in_with_password({
+            "email": data.email,
+            "password": data.password
+        })
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,57 +1,20 @@
-from fastapi import APIRouter, Depends, HTTPException
-
-from app.core.supabase import supabase
+from fastapi import APIRouter
 from app.schemas.auth import RegisterRequest, LoginRequest
+from app.services.auth_service import AuthService
 
 router = APIRouter(
     prefix="/auth",
     tags=["Autenticacion"],
 )
 
+auth_service = AuthService()
+
+
 @router.post("/register")
 def register(data: RegisterRequest):
-
-    response = supabase.auth.sign_up({
-        "email": data.email,
-        "password": data.password,
-        "options": {
-            "data": {
-                "nombre_completo": data.nombre_completo,
-                "telefono": data.telefono,
-                "ubicacion": data.ubicacion
-            }
-        }
-    })
-
-    if not response.user:
-        raise HTTPException(
-            status_code=400,
-            detail="No se pudo crear el usuario"
-        )
-
-    return {
-        "message": "Usuario registrado correctamente",
-        "user_id": response.user.id
-    }
+    return auth_service.register(data)
 
 
 @router.post("/login")
 def login(data: LoginRequest):
-
-    response = supabase.auth.sign_in_with_password({
-        "email": data.email,
-        "password": data.password
-    })
-
-    if not response.user or not response.session:
-        raise HTTPException(
-            status_code=401,
-            detail="Credenciales incorrectas"
-        )
-
-    return {
-        "message": "Inicio de sesión exitoso",
-        "access_token": response.session.access_token,
-        "refresh_token": response.session.refresh_token,
-        "user_id": response.user.id
-    }
+    return auth_service.login(data)
